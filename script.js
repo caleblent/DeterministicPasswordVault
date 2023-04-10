879b15d08b57a03cd7da43ffac4139ba6b56f771
caleblent <caleb@stcroixwebagency.com>
2023-04-09 21:05:33 -0500

    moved current to script2, now script has working code

--- script.js
+++ script.js
@@ -1,75 +1,38 @@
-// starting with the same file, I'm going to attempt to change this so that the
-// order of type of symbol is random as well
+// import keccak256 library
+const keccak256 = require('keccak256');
 
-const resultEl = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard')
+const seedPhrase = 'seed phrase words';
+const youtubeEl = document.getElementById('youtube');
 
-clipboardEl.addEventListener('click', () => {
-  const textarea = document.createElement('textarea')
-  const password = resultEl.innerText
+//  IIFE:
+//  Function that adds event listeners to YouTube row elements,
+//  which enables the password to be calculated if interacted with
+(function addYouTubeEventListeners() {
+    // 1) bring in the YouTube row elements
+    const siteNameEl = youtubeEl.querySelector('.site'); // probably don't need this
 
-  if (!password) {
-    return
-  }
+    const passwordResultEl = youtubeEl.querySelector('.password-result');
 
-  textarea.value = password
-  document.body.appendChild(textarea)
-  textarea.select()
-  document.execCommand('copy')
-  textarea.remove()
-  alert('Password copied to clipboard!')
-})
+    const numberEl = youtubeEl.querySelector('.number-entry');
 
-generateEl.addEventListener('click', () => {
-  const length = +lengthEl.value
-  const hasLower = lowercaseEl.checked
-  const hasUpper = uppercaseEl.checked
-  const hasNumber = numbersEl.checked
-  const hasSymbol = symbolsEl.checked
+    const uppercaseEl = youtubeEl.querySelector('.upper-checkbox');
+    const lowercaseEl = youtubeEl.querySelector('.lower-checkbox');
+    const numbersEl = youtubeEl.querySelector('.nums-checkbox');
+    const dollarSignEl = youtubeEl.querySelector('.dollar-sign-checkbox');
+    const atSignEl = youtubeEl.querySelector('.at-sign-checkbox');
+    const exclamationPointEl = youtubeEl.querySelector(
+        '.exclamation-point-checkbox'
+    );
 
-  resultEl.innerText = generatePassword2(
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol,
-    length
-  )
-})
+    // 2) get the phrase to be hashed
+    const phraseToHash =
+        seedPhrase + ' ' + siteNameEl.innerText + ' ' + numberEl.value;
 
-// modified password generator
-function generatePassword2(lower, upper, number, symbol, length) {
-  let generatedPassword = ''
-  let randomCharString = ''
+    console.log(phraseToHash);
 
-  if (lower) {
-    randomCharString += 'abcdefghijlkmnopqrstuvwxyz' // 26
-  }
-  if (upper) {
-    randomCharString += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' // 26
-  }
-  if (number) {
-    randomCharString += '1234567890' // 10
-  }
-  if (symbol) {
-    randomCharString += '!@#$%^&*(){}[]=<>/,.' // 20
-  }
+    // 3) hash the phrase (using keccak256?)
+    const hashedPhrase = keccak256(phraseToHash).toString('hex');
+    console.log(hashedPhrase);
 
-  for (let i = 0; i < length; i++) {
-    generatedPassword += getRandomChar(randomCharString)
-  }
-
-  const finalPassword = generatedPassword.slice(0, length)
-
-  return finalPassword
-}
-
-// Returns a random char from the string that's passed to the function
-function getRandomChar(charStr) {
-  return charStr[Math.floor(Math.random() * charStr.length)]
-}
+    // 4)
+})();
