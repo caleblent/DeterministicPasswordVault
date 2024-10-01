264a6470babef7b9c6a7b44557688de33e721f7d
caleblent <caleb@stcroixwebagency.com>
2024-10-01 18:24:02 -0500

    now dynamically adds table entries based on data in .txt file and updates correctly

--- js/script_dynamic.js
+++ js/script_dynamic.js
@@ -8,50 +8,17 @@ const seedPhraseEl = document.getElementById('seed-phrase');
 // const labelsEl = document.getElementById('labels'); might not need this tbh
 let currentSeedPhrase = seedPhraseEl.innerText;
 
-// Add button event listeners to the master seed phrase and all rows
-function addAllEventListeners(rowsList) {
-    // start at i=1 so we skip the #labels
-    for (let i = 1; i < rowsList.length; i++) {
-        addEventListeners(rowsList[i]);
-    }
-}
-
-// Add length entry, number entry, and copy button event listeners to one specific row
-function addEventListeners(row) {
-    // LENGTH --> modifies the number of characters in the password output
-    row.querySelector('.length-entry').addEventListener('click', () => {
-        calculatePassword(row);
-    });
-    // NUMBER --> generates a new "iteration"/password (goes from 0 to 100)
-    row.querySelector('.number-entry').addEventListener('click', () => {
-        calculatePassword(row);
-    });
-    // COPY button = copies the password string to clipboard
-    row.querySelector('.copy').addEventListener('click', () => {
-        const textarea = document.createElement('textarea');
-        const password = row.querySelector('.password-result').innerText;
-        if (!password) {
-            return;
-        }
-        textarea.value = password;
-        document.body.appendChild(textarea);
-        textarea.select();
-        document.execCommand('copy');
-        textarea.remove();
-    });
-}
-
 // Calculates all the passwords for each row by iteratively calling calculatePassword() on each row in the list
-function calculateAllPasswords(rowsList) {
+function calculateAllPasswords(rows) {
     // start at i=1 so we skip the #labels
-    for (let i = 1; i < rowsList.length; i++) {
-        calculatePassword(rowsList[i]);
+    for (let i = 1; i < rows.length; i++) {
+        calculatePassword(rows[i]);
     }
 }
 
 //  Calculates the password of a given row
 function calculatePassword(parentRowEl) {
-    // 1) bring in the YouTube row elements
+    // 1) bring in the row elements
     const siteNameEl = parentRowEl.querySelector('.site');
 
     const passwordResultEl = parentRowEl.querySelector('.password-result');
@@ -60,7 +27,6 @@ function calculatePassword(parentRowEl) {
     const numberEl = parentRowEl.querySelector('.number-entry');
 
     // 2) get the phrase to be hashed
-    console.log(seedPhraseEl.innerText);
     const phraseToHash =
         currentSeedPhrase + ' ' + siteNameEl.innerText + ' ' + numberEl.value;
 
@@ -71,8 +37,7 @@ function calculatePassword(parentRowEl) {
     passwordResultEl.innerText = hashedValue.slice(0, parseInt(lengthEl.value));
 }
 
-function addRows_JSON(data) {
-    console.log('data length: ' + data.length);
+function addRows(data) {
     for (let i = 0; i < data.length; i++) {
         // create row
         const row = document.createElement('div');
@@ -98,6 +63,11 @@ function addRows_JSON(data) {
         length.classList.add('length');
         // get the 'length' value from the JSON data
         length.innerHTML = `<input type="number" class="length-entry" min="0" max="64" value="${data[i].char_length}"/>`;
+        // add the event listener that recalculates password when interacted with
+        length.addEventListener('click', () => {
+            calculatePassword(row);
+        });
+        // add the element to the row
         row.appendChild(length);
 
         // add number
@@ -105,21 +75,39 @@ function addRows_JSON(data) {
         number.classList.add('number');
         // get the 'number' value from the JSON data
         number.innerHTML = `<input type="number" class="number-entry" min="0" max="20" value="${data[i].number}"/>`;
+        // add the event listener that recalculates password when interacted with
+        number.addEventListener('click', () => {
+            calculatePassword(row);
+        });
+        // add the element to the row
         row.appendChild(number);
 
-        // add copy
+        // add copy button
         const copy = document.createElement('div');
         copy.classList.add('copy');
         const clipboard = document.createElement('i');
         clipboard.classList.add('far');
         clipboard.classList.add('fa-clipboard');
-        // see if can combine the top 2 lines into 1
+        // add the elements to the row
         copy.appendChild(clipboard);
         row.appendChild(copy);
+        // event listener that copies the password to clipboard when clicked
+        copy.addEventListener('click', () => {
+            const textarea = document.createElement('textarea');
+            const password = row.querySelector('.password-result').innerText;
+            if (!password) {
+                return;
+            }
+            textarea.value = password;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand('copy');
+            textarea.remove();
+        });
 
+        // add this now completed row to the table
         container.appendChild(row);
     }
-    console.log('rows added!');
 }
 
 async function getData(data_file) {
@@ -127,21 +115,21 @@ async function getData(data_file) {
     let data_text = await data_object.text();
     let data = JSON.parse(data_text);
     // puts data into web page
-    addRows_JSON(data);
+    addRows(data);
 }
 
 // IIFE for running the program
 (() => {
     // reads data from .txt file, puts it into a data object, then uses that data object to add content to web page
     getData('js/data.txt');
-    // add event listeners to all rows
-    addAllEventListeners(container.getElementsByClassName('row'));
-    // calculate all row passwords
-    calculateAllPasswords(container.getElementsByClassName('row'));
     // add event listener to the seed phrase, which updates all passwords when modified
     seedPhraseEl.addEventListener('input', (e) => {
         currentSeedPhrase = e.target.value;
         // dynamically grabs all rows, since rows can be added/deleted by user
         calculateAllPasswords(container.getElementsByClassName('row'));
     });
+    // wait 100ms, then calculate all row passwords
+    setTimeout(() => {
+        calculateAllPasswords(container.getElementsByClassName('row'));
+    }, 100);
 })();
