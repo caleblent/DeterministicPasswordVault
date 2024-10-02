80cd84857f196df8a2ae9693839d1d67f8f92b8a
caleblent <caleb@stcroixwebagency.com>
2024-10-01 20:27:54 -0500

    added Add button which does exactly what is sounds like

--- js/script_dynamic.js
+++ js/script_dynamic.js
@@ -4,14 +4,15 @@
 
 // needed variables that won't get deleted
 const container = document.querySelector('.flexbox-container');
+const rowsContainer = container.querySelector('.rows');
 const seedPhraseEl = document.getElementById('seed-phrase');
+const addRowEl = document.getElementById('add-row');
 // const labelsEl = document.getElementById('labels'); might not need this tbh
 let currentSeedPhrase = seedPhraseEl.innerText;
 
 // Calculates all the passwords for each row by iteratively calling calculatePassword() on each row in the list
 function calculateAllPasswords(rows) {
-    // start at i=1 so we skip the #labels
-    for (let i = 1; i < rows.length; i++) {
+    for (let i = 0; i < rows.length; i++) {
         calculatePassword(rows[i]);
     }
 }
@@ -39,91 +40,132 @@ function calculatePassword(parentRowEl) {
 
 function addRows(data) {
     for (let i = 0; i < data.length; i++) {
-        // create row
-        const row = document.createElement('div');
-        // add .row class to it
-        row.classList.add('row');
-
-        // add site
-        const site = document.createElement('div');
-        site.classList.add('site');
-        // get the 'site' value from the JSON data
-        site.innerText = data[i].site;
-        row.appendChild(site);
-
-        // add passowrd // add password-result
-        const password = document.createElement('div');
-        password.classList.add('password');
-        password.innerHTML =
-            '<span class="password-result" disabled>null</span>';
-        row.appendChild(password);
-
-        // add length
-        const length = document.createElement('div');
-        length.classList.add('length');
-        // get the 'length' value from the JSON data
-        length.innerHTML = `<input type="number" class="length-entry" min="0" max="64" value="${data[i].char_length}"/>`;
-        // add the event listener that recalculates password when interacted with
-        length.addEventListener('click', () => {
-            calculatePassword(row);
-        });
-        // add the element to the row
-        row.appendChild(length);
-
-        // add number
-        const number = document.createElement('div');
-        number.classList.add('number');
-        // get the 'number' value from the JSON data
-        number.innerHTML = `<input type="number" class="number-entry" min="0" max="20" value="${data[i].number}"/>`;
-        // add the event listener that recalculates password when interacted with
-        number.addEventListener('click', () => {
-            calculatePassword(row);
-        });
-        // add the element to the row
-        row.appendChild(number);
-
-        // add copy button
-        const copyButton = document.createElement('div');
-        copyButton.classList.add('copy');
-        // event listener that copies the password to clipboard when clicked
-        copyButton.addEventListener('click', () => {
-            const textarea = document.createElement('textarea');
-            const password = row.querySelector('.password-result').innerText;
-            if (!password) {
-                return;
-            }
-            textarea.value = password;
-            document.body.appendChild(textarea);
-            textarea.select();
-            document.execCommand('copy');
-            textarea.remove();
-        });
-        // add clipboard icon
-        const clipboard = document.createElement('i');
-        clipboard.classList.add('far');
-        clipboard.classList.add('fa-clipboard');
-        copyButton.appendChild(clipboard);
-        // add the element to the row
-        row.appendChild(copyButton);
-
-        // add delete button
-        const deleteButton = document.createElement('div');
-        deleteButton.classList.add('delete');
-        // add event listener that deletes row when clicked
-        deleteButton.addEventListener('click', () => {
-            row.remove();
-        });
-        // add trash can icon
-        const trashCan = document.createElement('i');
-        trashCan.classList.add('fa-solid');
-        trashCan.classList.add('fa-trash-can');
-        deleteButton.appendChild(trashCan);
-        // add delete element to row
-        row.appendChild(deleteButton);
-
-        // add this now completed row to the table
-        container.appendChild(row);
+        addNewRow(data[i].site, data[i].char_length, data[i].number);
     }
+}
+
+function addNewRow(site, char_length, number) {
+    // create row
+    const row = document.createElement('div');
+    // add .row class to it
+    row.classList.add('row');
+
+    // add site
+    const siteEl = document.createElement('div');
+    siteEl.classList.add('site');
+    // get the 'site' value from the JSON data
+    siteEl.innerText = site;
+    row.appendChild(siteEl);
+
+    // add passowrd // add password-result
+    const passwordEl = document.createElement('div');
+    passwordEl.classList.add('password');
+    passwordEl.innerHTML = '<span class="password-result" disabled>null</span>';
+    row.appendChild(passwordEl);
+
+    // add length
+    const lengthEl = document.createElement('div');
+    lengthEl.classList.add('length');
+    // get the 'length' value from the JSON data
+    lengthEl.innerHTML = `<input type="number" class="length-entry" min="0" max="64" value="${char_length}"/>`;
+    // add the event listener that recalculates password when interacted with
+    lengthEl.addEventListener('click', () => {
+        calculatePassword(row);
+    });
+    // add the element to the row
+    row.appendChild(lengthEl);
+
+    // add number
+    const numberEl = document.createElement('div');
+    numberEl.classList.add('number');
+    // get the 'number' value from the JSON data
+    numberEl.innerHTML = `<input type="number" class="number-entry" min="0" max="20" value="${number}"/>`;
+    // add the event listener that recalculates password when interacted with
+    numberEl.addEventListener('click', () => {
+        calculatePassword(row);
+    });
+    // add the element to the row
+    row.appendChild(numberEl);
+
+    // add copy button
+    const copyButton = document.createElement('div');
+    copyButton.classList.add('copy');
+    // event listener that copies the password to clipboard when clicked
+    copyButton.addEventListener('click', () => {
+        const textarea = document.createElement('textarea');
+        const password = row.querySelector('.password-result').innerText;
+        if (!password) {
+            return;
+        }
+        textarea.value = password;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        textarea.remove();
+    });
+    // add clipboard icon
+    const clipboard = document.createElement('i');
+    clipboard.classList.add('far');
+    clipboard.classList.add('fa-clipboard');
+    copyButton.appendChild(clipboard);
+    // add the element to the row
+    row.appendChild(copyButton);
+
+    // add delete button
+    const deleteButton = document.createElement('div');
+    deleteButton.classList.add('delete');
+    // add event listener that deletes row when clicked
+    deleteButton.addEventListener('click', () => {
+        row.remove();
+    });
+    // add trash can icon
+    const trashCan = document.createElement('i');
+    trashCan.classList.add('fa-solid');
+    trashCan.classList.add('fa-trash-can');
+    deleteButton.appendChild(trashCan);
+    // add delete element to row
+    row.appendChild(deleteButton);
+
+    // add this now completed row to the table
+    rowsContainer.appendChild(row);
+
+    // return row (if needed)
+    return row;
+}
+
+function addAddRow() {
+    // create add row
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.setAttribute('id', 'add-row');
+
+    // add-site input
+    const addSite = document.createElement('div');
+    addSite.classList.add('site');
+    addSite.innerHTML = '<input type="text" />';
+    row.appendChild(addSite);
+
+    // 3 blank spots
+    row.appendChild(document.createElement('div'));
+    row.appendChild(document.createElement('div'));
+    row.appendChild(document.createElement('div'));
+
+    // add button itself
+    const addButton = document.createElement('div');
+    addButton.innerText = 'Add';
+    addButton.classList.add('add-button');
+    addButton.addEventListener('click', () => {
+        newValue = row.querySelector('input').value;
+        newRow = addNewRow(newValue, 10, 0);
+        calculatePassword(newRow);
+        row.querySelector('input').value = '';
+    });
+    row.appendChild(addButton);
+
+    // delete button?
+    // row.appendChild(document.createElement('div'));
+
+    container.append(row);
 }
 
 async function getData(data_file) {
@@ -142,10 +184,11 @@ async function getData(data_file) {
     seedPhraseEl.addEventListener('input', (e) => {
         currentSeedPhrase = e.target.value;
         // dynamically grabs all rows, since rows can be added/deleted by user
-        calculateAllPasswords(container.getElementsByClassName('row'));
+        calculateAllPasswords(rowsContainer.getElementsByClassName('row'));
     });
     // wait 100ms, then calculate all row passwords
     setTimeout(() => {
-        calculateAllPasswords(container.getElementsByClassName('row'));
+        calculateAllPasswords(rowsContainer.getElementsByClassName('row'));
+        addAddRow();
     }, 100);
 })();
